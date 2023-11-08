4d8978fec32ad63ada8dd93779f6316abb581633
amankasahun <52367271+amankasahun@users.noreply.github.com>
2023-11-08 15:47:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> aman and save to reload. reactsssss
+          Edit <code>src/App.js</code> aman and save to reload. reacts
         </p>
         <a
           className="App-link"
